920e785ce2c9b1fa7aa414bd5f098569a5b59a91
Egor Shmakov <seaplanepawn@gmail.com>
2026-07-14 08:28:57 +0300

    robots and sitemap added

--- script.js
+++ script.js
@@ -242,3 +242,12 @@ const fadeObserver = new IntersectionObserver(
 );
 
 document.querySelectorAll('.fade-in').forEach((element) => fadeObserver.observe(element));
+
+(function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110604711', 'ym');
+
+    ym(110604711, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
